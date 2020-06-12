0bdd30466cab153ef1bdaf8035eeca281477102f
darshankumar <darshannbhatia85@gmail.com>
2020-06-12 14:12:09 -0400

    Java 11 HttpClient Example

---
+++ JavaEight_Example/src/com/learn/java_eleven/httpClient/AsyncHttpClientExample.java
@@ -0,0 +1,24 @@
+package com.learn.java_eleven.httpClient;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class AsyncHttpClientExample {
+
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://www.google.com")).build();
+
+		CompletableFuture<HttpResponse<String>> output = client.sendAsync(request,
+				HttpResponse.BodyHandlers.ofString());
+
+		System.out.println(output.get().body());
+
+	}
+
+}
