996b57f0cfb7b63a6bb7524002e5b3b52db4442e
darshankumar <darshannbhatia85@gmail.com>
2020-06-30 09:52:37 -0400

    https://leetcode.com/problems/path-sum/submissions/

--- Coding-Interview-Questions/src/leetcode/question/path_sum/Solution.java
+++ Coding-Interview-Questions/src/leetcode/question/path_sum/Solution.java
@@ -2,6 +2,8 @@ package leetcode.question.path_sum;
 
 class Solution {
 
+	static boolean flag = false;
+
 	public static boolean hasPathSum(TreeNode root, int sum) {
 		if (root == null) {
 			return false;
@@ -12,6 +14,44 @@ class Solution {
 		}
 
 		return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+	}
+
+	public static boolean hasPathSum2(TreeNode root, int sum) {
+		if (root == null) {
+			return false;
+		}
+		boolean answer = hasPathSumhelper(root, 0, sum);
+		flag = false;
+		return answer;
+	}
+
+	private static boolean hasPathSumhelper(TreeNode root, int current_sum, int sum) {
+
+		// if it's leaf node & check sum
+		if (root.right == null && root.left == null) {
+			if (root.val + current_sum == sum) {
+				flag = true;
+			}
+		}
+
+		// check for right tree
+		if (root.right != null && root.left == null) {
+			hasPathSumhelper(root.right, root.val + current_sum, sum);
+		}
+
+		// check for left tree
+		if (root.right == null && root.left != null) {
+			hasPathSumhelper(root.left, root.val + current_sum, sum);
+		}
+
+		// check for both ends
+
+		if (root.right != null && root.left != null) {
+			hasPathSumhelper(root.right, root.val + current_sum, sum);
+			hasPathSumhelper(root.left, root.val + current_sum, sum);
+		}
+
+		return flag;
 	}
 
 	public class TreeNode {
