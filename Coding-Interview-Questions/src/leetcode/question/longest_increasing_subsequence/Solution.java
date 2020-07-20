587cdabd3950d8804f2ed79ffe789957b274e52b
darshankumar <darshannbhatia85@gmail.com>
2020-07-20 09:26:10 -0400

    https://leetcode.com/problems/longest-increasing-subsequence/

--- Coding-Interview-Questions/src/leetcode/question/longest_increasing_subsequence/Solution.java
+++ Coding-Interview-Questions/src/leetcode/question/longest_increasing_subsequence/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
 
 	public static int lengthOfLIS(int[] nums) {
 		int len = nums.length;
-		if(len==0) {
+		if (len == 0) {
 			return 0;
 		}
 		int[] lis = new int[len];
@@ -22,6 +22,23 @@ public class Solution {
 		}
 		Arrays.sort(lis);
 		return lis[len - 1];
+	}
+
+	public int lengthOfLIS2(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
+		int[] max = new int[nums.length];
+		Arrays.fill(max, 1);
+		int result = 1;
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[i] > nums[j]) {
+					max[i] = Math.max(max[i], max[j] + 1);
+				}
+			}
+			result = Math.max(max[i], result);
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
